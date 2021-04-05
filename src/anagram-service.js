9fcc89a80d17367d4b1e0d0b00e26bc632697faf
Karl Tiedt <ktiedt@gmail.com>
2021-04-05 15:54:31 -0500

    Coding exercise completed by Karl Tiedt

--- src/anagram-service.js
+++ src/anagram-service.js
@@ -6,49 +6,77 @@ const fs = require('fs');
  */
 class AnagramService {
 
-  /**
-   * Creates an AnagramService instance
-   * @param {string} dictionaryFilePath Path to the dictionary file
-   */
-  constructor(dictionaryFilePath) {
-    this.dictionaryFilePath = dictionaryFilePath;
-    this.wordsMap = new Map();
-  }
+	/**
+	 * Creates an AnagramService instance
+	 * @param {string} dictionaryFilePath Path to the dictionary file
+	 */
+	constructor(dictionaryFilePath) {
+		this.dictionaryFilePath = dictionaryFilePath;
+		this.wordsMap = new Map();
+		this.lines = [];
+	}
 
-  /**
-   * Parses and loads the dictionary
-   * @returns Promise containing the initialized AnagramService
-   */
-  async loadDictionary() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(this.dictionaryFilePath, (err, data) => {
-        if (err) {
-          return reject(err);
-        }
+	/**
+	 * Parses and loads the dictionary
+	 * @returns Promise containing the initialized AnagramService
+	 */
+	async loadDictionary() {
+		return new Promise((resolve, reject) => {
+			fs.readFile(this.dictionaryFilePath, (err, data) => {
+				if (err) {
+					return reject(err);
+				}
 
-        const lines = data.toString().split('\n');
+				// const lines = this.lines = data.toString().split('\n');
+				this.lines = data.toString().split('\n');
 
-        lines.forEach((line) => {
-          this.wordsMap.set(line.toLowerCase(), [line]); 
-        });
-        return resolve(this);
-      });
-    });
-  }
+				// lines.forEach((line) => {
+				// 	this.wordsMap.set(line.toLowerCase(), this.processAnagrams(line));
+				// });
 
-  /**
-   * Returns all anagrams for the given term
-   * @param {string} term The term to find anagrams for 
-   * @returns A string[] of anagram matches
-   */
-  async getAnagrams(term) {
-    if (!this.wordsMap || this.wordsMap.size === 0) {
-      throw Error('Error: Dictionary not initialized');
-    }
+				return resolve(this);
+			});
+		});
+	}
 
-    // TODO: The anagram lookup 🤦‍♂️
-    return this.wordsMap.get(term);
-  }
+	processAnagrams(word) {
+		const letters = word.split('');
+		// test is the word being checked from the original lines array.
+		// word is the current word we are finding anagrams for.
+		const results = this.lines.filter((test) => {
+			if (word.length === test.length) {
+				const tested = {};
+				return letters.every((letter) => {
+					if (typeof tested[letter] === 'undefined') {
+						const duplicatesRE = new RegExp(`${letter}`, 'ig');
+						const duplicatesTest = test.match(duplicatesRE) || [];
+						const duplicatesLine = word.match(duplicatesRE) || [];
+						tested[letter] = duplicatesTest.length === duplicatesLine.length;
+					}
+
+					return tested[letter];
+				});
+			}
+
+			return false;
+		});
+
+		// console.log(`Matches for ${word}: `, results.join(', '));
+		return results;
+	}
+	/**
+	 * Returns all anagrams for the given term
+	 * @param {string} term The term to find anagrams for 
+	 * @returns A string[] of anagram matches
+	 */
+	async getAnagrams(term) {
+		if (!this.lines || this.lines.length === 0) {
+			throw Error('Error: Dictionary not initialized');
+		}
+
+		// TODO: The anagram lookup 🤦‍♂️
+		return this.processAnagrams(term);
+	}
 }
 
 module.exports = AnagramService;
